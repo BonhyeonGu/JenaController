fdc7237e2ba81d219a0f340d2d3485e0dc04c128
whwhdtk <8whwhdtk8@naver.com>
2023-08-10 13:51:35 +0900

    온톨로지 읽고 유효성검증 하는 함수 추가

--- app/src/main/java/jenacontroller/ImportRDF_MakeModel.java
+++ app/src/main/java/jenacontroller/ImportRDF_MakeModel.java
@@ -1,17 +1,54 @@
 package jenacontroller;
+//-------------------------------------
 import org.apache.jena.rdf.model.*;
-//import org.apache.jena.tdb.TDBFactory;
+import org.apache.jena.tdb.TDBFactory;
+import org.apache.jena.query.Dataset;
+//-------------------------------------
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.reasoner.ValidityReport;
+import java.util.Iterator;
+//-------------------------------------
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class ImportRDF_MakeModel {
     private Model model;
+    private static final Logger logger = LoggerFactory.getLogger(ImportRDF_MakeModel.class);
 
     public ImportRDF_MakeModel(String path, String format) {
-        this.model = ModelFactory.createDefaultModel();
-        //this.model = TDBFactory.createDataset("directory");
-        this.model.read(path, format);
+        //this.model = ModelFactory.createDefaultModel();
+        //this.model.read(path, format);
+        Dataset dataset = TDBFactory.createDataset("./_db");
+        model = dataset.getDefaultModel();
+        model.read(path, format);
+    }
+
+    public void getLogInfo() {
+        logger.info("This is an information message.");
     }
 
     public Model getModel() {
+        return this.model;
+    }
+
+    public Model validationTest(String owl) {
+        OntModel ontModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM_RULE_INF);
+        ontModel.read(owl);
+        InfModel infModel = ModelFactory.createInfModel(ontModel.getReasoner(), ontModel);
+        ValidityReport validityReport = infModel.validate();
+        if (ontModel.validate().isValid()) {
+            System.out.println("Ontology is valid");
+        }
+        else {
+            System.out.println("Ontology is NOT valid");
+            Iterator<ValidityReport.Report> i = validityReport.getReports();
+            while (i.hasNext()) {
+                ValidityReport.Report report = i.next();
+                System.out.println(" - " + report.getDescription());
+            }
+        }
         return this.model;
     }
 }
