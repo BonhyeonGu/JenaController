682b2d99b8d8471042d748b8f250697ed923169d
BonhyeonGu <bonhyeon.gu@9bon.org>
2023-08-17 20:58:42 +0900

    센서띵스는 URL로, RDF검증추가

--- src/main/kotlin/JenaController/WebController.kt
+++ src/main/kotlin/JenaController/WebController.kt
@@ -17,7 +17,8 @@ class WebController {
     @GetMapping("/test")
     fun test(model: Model): String {
         val jenaValidate = JenaValidate()
-        val result = jenaValidate.validationTest_OWL("sensorthings.owl")
+        val result = jenaValidate.validationTest_OWL("https://paper.9bon.org/ontologies/sensorthings/1.1")
+        jenaValidate.validationTest_OWLandRDF("https://paper.9bon.org/ontologies/sensorthings/1.1", "out.rdf")
         model.addAttribute("message", result)
         return "index"
     }
